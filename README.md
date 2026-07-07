089cdab746537ba73649af21853b133702e17201
Vanza Setia <venusbumi2@gmail.com>
2026-07-07 14:39:16 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Vanza Notepad
+
+Vanza Notepad is a part of Tiny Project made by me, Vanza Setia.
+
+## What are the benefits?
+
+- No ads.
+- No trackers.
+- No proprietary JavaScript.
+- Zero bloat.
+- Written in HTML, CSS, and JavaScript.
+- Hackable.
+- Use Grammarly browser extension easily.
+- Built to last for _at least_ 30 years.
+- Free web app.
---
+++ sw.js
@@ -0,0 +1,94 @@
+// Source: https://github.com/mdn/dom-examples/blob/main/service-worker/simple-service-worker/sw.js
+/*
+Copyright 2026 Vanza Setia
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
+
+const putInCache = async (request, response) => {
+  const cache = await caches.open("v1");
+  await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
+  // First try to get the resource from the cache
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+
+  // Next try to use the preloaded response, if it's there
+  // NOTE: Chrome throws errors regarding preloadResponse, see:
+  // https://bugs.chromium.org/p/chromium/issues/detail?id=1420515
+  // https://github.com/mdn/dom-examples/issues/145
+  // To avoid those errors, remove or comment out this block of preloadResponse
+  // code along with enableNavigationPreload() and the "activate" listener.
+  const preloadResponse = await preloadResponsePromise;
+  if (preloadResponse) {
+    console.info("using preload response", preloadResponse);
+    putInCache(request, preloadResponse.clone());
+    return preloadResponse;
+  }
+
+  // Next try to get the resource from the network
+  try {
+    const responseFromNetwork = await fetch(request.clone());
+    // response may be used only once
+    // we need to save clone to put one copy in cache
+    // and serve second one
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    const fallbackResponse = await caches.match(fallbackUrl);
+    if (fallbackResponse) {
+      return fallbackResponse;
+    }
+    // when even the fallback response is not available,
+    // there is nothing we can do, but we must always
+    // return a Response object
+    return new Response("Network error happened", {
+      status: 408,
+      headers: { "Content-Type": "text/plain" }
+    });
+  }
+};
+
+const enableNavigationPreload = async () => {
+  if (self.registration.navigationPreload) {
+    // Enable navigation preloads!
+    await self.registration.navigationPreload.enable();
+  }
+};
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(enableNavigationPreload());
+});
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(addResourcesToCache(["./", "./index.html"]));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    cacheFirst({
+      request: event.request,
+      preloadResponsePromise: event.preloadResponse,
+      fallbackUrl: "/"
+    })
+  );
+});
